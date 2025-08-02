b5e80ebd0150ba427285b35f9be9ddfc8684c684
ArafatHasib <mdmahimd689@gmail.com>
2025-08-02 23:14:12 +0600

    Add files via upload

---
+++ seup.py
@@ -0,0 +1,73 @@
+import os #(স্ক্রিন clear করার ফাংশন)
+import sys #(Tipy writer ফাংশন )
+import time #(সময় ফাংশন )
+
+
+os.system("clear")
+
+
+print ("\n\n\n")
+
+ab="     System Lodding... "
+for c in ab:
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    time.sleep(0.1)
+print("\n\n\n")
+
+
+time.sleep(2)
+os.system("clear")  
+print("\n\n\n")
+ab="     \033[1;32mSuccessfully Lodding...\n"
+for c in ab:
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    time.sleep(0.1)
+    
+time.sleep(1)   
+print("\n\n")
+name =input("    \033[1;33mEnter Your Name:  ")
+
+
+ab="    \033[1;32mHello "+name+",   \033[1;34mWellcome to Our poggram..."
+for c in ab:
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    time.sleep(0.1)
+os.system("clear")
+time.sleep(2)
+    
+   
+print("""
+
+
+  ______   __      __  _______   ________  _______         _______    ______   __    __  ________ 
+ /      \\ /  \    /  |/       \\ /        |/       \\       /       \  /      \ /  \\ /  |/        |
+/$$$$$$  |$$  \\  /$$/ $$$$$$$  |$$$$$$$$/ $$$$$$$  |      $$$$$$$  |/$$$$$$  |$$  \\ $$ |$$$$$$$$/ 
+$$ |  $$/  $$  \\/$$/  $$ |__$$ |$$ |__    $$ |__$$ |      $$ |__$$ |$$ |  $$ |$$$  \$$ |$$ |__    
+$$ |        $$  $$/   $$    $$< $$    |   $$    $$<       $$    $$< $$ |  $$ |$$$$  $$ |$$    |   
+$$ |   __    $$$$/    $$$$$$$  |$$$$$/    $$$$$$$  |      $$$$$$$  |$$ |  $$ |$$ $$ $$ |$$$$$/    
+$$ \__/  |    $$ |    $$ |__$$ |$$ |_____ $$ |  $$ |      $$ |  $$ |$$ \__$$ |$$ |$$$$ |$$ |_____ 
+$$    $$/     $$ |    $$    $$/ $$       |$$ |  $$ |      $$ |  $$ |$$    $$/ $$ | $$$ |$$       |
+ $$$$$$/      $$/     $$$$$$$/  $$$$$$$$/ $$/   $$/       $$/   $$/  $$$$$$/  $$/   $$/ $$$$$$$$/ 
+                                                                                                                       
+
+\033[1;35m==================================================================
+Tools Owner  : Abdullah Fahad 
+Team            :Muslim fighter 
+GitHub          :$$$$$$$$$$
+Email             :mdabdullah@gmail.com
+mobile           :0176876767667
+Use Only for legal work Not Use For Ileg work
+
+\033[1;35m==================================================================
+                                                                      
+                                                                      
+                                                                      
+
+""")
+    
+    
+    
+
